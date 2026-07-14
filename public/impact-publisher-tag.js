dfda5c63b9e5b83cfab58d8cc90ac0bcfe1a76ff
olstaylor <ollietaylor2001@gmail.com>
2026-07-14 10:53:11 +0100

    feat: add account Publisher Tag loader

---
+++ public/impact-publisher-tag.js
@@ -0,0 +1,2 @@
+// Impact Publisher Tag for TourTicketCompare.
+(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})("https://utt.impactcdn.com/P-A3977745-d128-4905-97f4-b5b676ba4a171.js","script","impactStat",document,window);impactStat("trackImpression");

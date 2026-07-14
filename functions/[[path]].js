5b7762727b5310f2c2500af9974475906298968c
olstaylor <ollietaylor2001@gmail.com>
2026-07-14 13:09:09 +0100

    Add guide-to-guide event comparison link

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1522,7 +1522,11 @@ function renderMainContent(route, catalog, events = [], guideContent = {}, env =
       route.description
     )}</p>${renderGuideProvenance(route)}${contentHtml}${renderGuideSources(
       guideContent[route.path]?.sources
-    )}${artistBrowseHtml}<div class="action-row">${anchor(\n      "Compare event ticket prices",\n      "/guides/how-to-compare-event-ticket-prices",\n      "button button-primary"\n    )}${anchor(
+    )}${artistBrowseHtml}<div class="action-row">${anchor(
+      "Compare event ticket prices",
+      "/guides/how-to-compare-event-ticket-prices",
+      "button button-primary"
+    )}${anchor(\n      "Compare event ticket prices",\n      "/guides/how-to-compare-event-ticket-prices",\n      "button button-primary"\n    )}${anchor(
       "Compare concert ticket prices",
       "/compare-concert-ticket-prices",
       "button button-primary"
